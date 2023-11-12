87c4308c4e115c90c4660036179468023d25e8a3
farry-rangian <farry12angian@gmail.com>
2023-11-12 21:20:05 +0800

    make new ui using new template

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,100 +1,177 @@
-<aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-    <div class="card">
-      <div class="sidenav-header">
-        <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-          <img src="{{ asset('img/sismonalisa.png')}}" class="navbar-brand-img h-100" alt="main_logo">
-          <span class="ms-1 font-weight-bold">SISMONALISA</span>
-        </a>
-      </div>
-    
-      <hr class="horizontal dark mt-0">
-      <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" href="{{Auth::user()->role}}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Dashboard</span>
-            </a>
-          </li>
-          @if (Auth::user()->role == 'admin')
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('datadesas.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-world-2 text-danger text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Data Desa</span>
-            </a>
-          </li>
-          @endif
-          <li class="nav-item mt-3">
-            <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Pengawasan Rutin</h6>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('programs.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-single-copy-04 text-warning text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Realisasi Program</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('anggarans.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Realisasi Anggaran</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('keuntungans.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-collection text-info text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Pemanfaatan Keuntungan</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('pelaporans.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-collection text-info text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Pelaporan</span>
-            </a>
-          </li>
-          <li class="nav-item mt-3">
-            <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Pengawasan Tahunan</h6>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('pembagians.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-single-02 text-dark text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Pembagian Laba Tahunan</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('kesimpulans.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-collection text-info text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Kesimpulan Tahunan</span>
-            </a>
-          </li>
-          <li class="nav-item mt-3">
-            <h6 class="ps-4 ms-2 text-uppercase text-xs font-weight-bolder opacity-6">Pengawasan Insidentil</h6>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link " href="{{ route('pengawasans.index') }}">
-              <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
-                <i class="ni ni-collection text-info text-sm opacity-10"></i>
-              </div>
-              <span class="nav-link-text ms-1">Pengawasan Insidentil</span>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </aside>
+<!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="dlabnav">
+            <div class="dlabnav-scroll">
+				<ul class="metismenu" id="menu">
+                    <li><a class="" href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-home"></i>
+							<span class="nav-text">Dashboard</span>
+						</a>
+                    </li>
+                    @if (Auth::user()->role == 'admin')
+                    <li><a href="{{route('datadesas.index')}}" class="" aria-expanded="false">
+                            <i class="fas fa-user-check"></i>
+                            <span class="nav-text">Data Desa</span>
+                        </a>
+                    </li> 
+                    @endif
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+						<i class="fas fa-info-circle"></i>
+							<span class="nav-text">Pengawasan Rutin</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Semester</a>
+                                <ul aria-expanded="false">
+                                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">1</a>
+                                    <ul aria-expanded="false">
+                                        <li><a href="email-compose.html">Realisasi Anggaran</a></li>
+                                        <li><a href="email-inbox.html">Realisasi Program</a></li>
+                                        <li><a href="email-inbox.html">Pemanfaatan Keuntungan</a></li>
+                                        <li><a href="email-inbox.html">Pelaporan</a></li>
+                                    </ul>
+                                    </li>
+                                    <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">2</a>
+                                        <ul aria-expanded="false">
+                                            <li><a href="email-compose.html">Realisasi Anggaran</a></li>
+                                            <li><a href="email-inbox.html">Realisasi Program</a></li>
+                                            <li><a href="email-inbox.html">Pemanfaatan Keuntungan</a></li>
+                                            <li><a href="email-inbox.html">Pelaporan</a></li>
+                                        </ul>
+                                    </li>
+                                </ul>
+                            </li>
+							<li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Tahunan</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="ecom-product-grid.html">Pembagian Laba Tahunan</a></li>
+									<li><a href="ecom-product-list.html">Kesimpulan Tahunan</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-chart-line"></i>
+							<span class="nav-text">Charts</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="chart-flot.html">Flot</a></li>
+                            <li><a href="chart-morris.html">Morris</a></li>
+                            <li><a href="chart-chartjs.html">Chartjs</a></li>
+                            <li><a href="chart-chartist.html">Chartist</a></li>
+                            <li><a href="chart-sparkline.html">Sparkline</a></li>
+                            <li><a href="chart-peity.html">Peity</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fab fa-bootstrap"></i>
+							<span class="nav-text">Bootstrap</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="ui-accordion.html">Accordion</a></li>
+                            <li><a href="ui-alert.html">Alert</a></li>
+                            <li><a href="ui-badge.html">Badge</a></li>
+                            <li><a href="ui-button.html">Button</a></li>
+                            <li><a href="ui-modal.html">Modal</a></li>
+                            <li><a href="ui-button-group.html">Button Group</a></li>
+                            <li><a href="ui-list-group.html">List Group</a></li>
+                            <li><a href="ui-card.html">Cards</a></li>
+                            <li><a href="ui-carousel.html">Carousel</a></li>
+                            <li><a href="ui-dropdown.html">Dropdown</a></li>
+                            <li><a href="ui-popover.html">Popover</a></li>
+                            <li><a href="ui-progressbar.html">Progressbar</a></li>
+                            <li><a href="ui-tab.html">Tab</a></li>
+                            <li><a href="ui-typography.html">Typography</a></li>
+                            <li><a href="ui-pagination.html">Pagination</a></li>
+                            <li><a href="ui-grid.html">Grid</a></li>
+
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-heart"></i>
+							<span class="nav-text">Plugins</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="uc-select2.html">Select 2</a></li>
+                            <li><a href="uc-nestable.html">Nestedable</a></li>
+                            <li><a href="uc-noui-slider.html">Noui Slider</a></li>
+                            <li><a href="uc-sweetalert.html">Sweet Alert</a></li>
+                            <li><a href="uc-toastr.html">Toastr</a></li>
+                            <li><a href="map-jqvmap.html">Jqv Map</a></li>
+							<li><a href="uc-lightgallery.html">Light Gallery</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-file-alt"></i>
+							<span class="nav-text">Forms</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="form-element.html">Form Elements</a></li>
+                            <li><a href="form-wizard.html">Wizard</a></li>
+                            <li><a href="form-ckeditor.html">CkEditor</a></li>
+                            <li><a href="form-pickers.html">Pickers</a></li>
+                            <li><a href="form-validation.html">Form Validate</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-table"></i>
+							<span class="nav-text">Table</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="table-bootstrap-basic.html">Bootstrap</a></li>
+                            <li><a href="table-datatable-basic.html">Datatable</a></li>
+                        </ul>
+                    </li>
+                    <li><a class="has-arrow " href="javascript:void()" aria-expanded="false">
+							<i class="fas fa-clone"></i>
+							<span class="nav-text">Pages</span>
+						</a>
+                        <ul aria-expanded="false">
+                            <li><a href="page-login.html">Login</a></li>
+                            <li><a href="page-register.html">Register</a></li>
+                            <li><a class="has-arrow" href="javascript:void()" aria-expanded="false">Error</a>
+                                <ul aria-expanded="false">
+                                    <li><a href="page-error-400.html">Error 400</a></li>
+                                    <li><a href="page-error-403.html">Error 403</a></li>
+                                    <li><a href="page-error-404.html">Error 404</a></li>
+                                    <li><a href="page-error-500.html">Error 500</a></li>
+                                    <li><a href="page-error-503.html">Error 503</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="page-lock-screen.html">Lock Screen</a></li>
+                            <li><a href="empty-page.html">Empty Page</a></li>
+                        </ul>
+                    </li>
+                </ul>
+				<div class="side-bar-profile">
+					<div class="d-flex align-items-center justify-content-between mb-3">
+						<div class="side-bar-profile-img">
+							<img src="images/user.jpg" alt="">
+						</div>
+						<div class="profile-info1">
+							<h4 class="fs-18 font-w500">Soeng Souy</h4>
+							<span>example@mail.com</span>
+						</div>
+						<div class="profile-button">
+							<i class="fas fa-caret-down scale5 text-light"></i>
+						</div>
+					</div>	
+					<div class="d-flex justify-content-between mb-2 progress-info">
+						<span class="fs-12"><i class="fas fa-star text-orange me-2"></i>Task Progress</span>
+						<span class="fs-12">20/45</span>
+					</div>
+					<div class="progress default-progress">
+						<div class="progress-bar bg-gradientf progress-animated" style="width: 45%; height:10px;" role="progressbar">
+							<span class="sr-only">45% Complete</span>
+						</div>
+					</div>
+				</div>
+				
+				<div class="copyright">
+					<p><strong>Fillow Saas Admin</strong> © 2021 All Rights Reserved</p>
+					<p class="fs-12">Made with <span class="heart"></span> by DexignLabs</p>
+				</div>
+			</div>
+        </div>
+        <!--**********************************
+            Sidebar end
+        ***********************************-->
